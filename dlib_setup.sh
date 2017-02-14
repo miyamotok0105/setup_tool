024c24aa92603b5650ab0336a22944ac3950bdd8
keiichiro miyamoto <keiichiro.miyamoto@uei.co.jp>
2017-02-14 16:47:18 +0900

    add dlib

---
+++ dlib_setup.sh
@@ -0,0 +1,13 @@
+
+#====================================================================
+# dlib
+#====================================================================
+
+# if don't get around this.
+# sudo apt-get install python-dev python-numpy
+# sudo apt-get install libboost-dev
+# sudo apt-get install libboost-python-dev
+# sudo apt-get install libboost-system-dev
+
+sudo pip install dlib
+
