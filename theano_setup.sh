5c37b2a3cd8f97a6e6ce4e8cfa39a39493309b96
keiichiro miyamoto <keiichiro.miyamoto@uei.co.jp>
2017-02-15 13:35:22 +0900

    add theano

---
+++ theano_setup.sh
@@ -0,0 +1,24 @@
+
+#====================================================================
+# theano
+#====================================================================
+
+#see Requirements.
+#if you see more.
+#http://deeplearning.net/software/theano/install.html
+
+
+yes | sudo pip install pydot-ng
+yes | sudo pip install Theano
+
+
+#====================================================================
+# theanets
+#====================================================================
+
+# sudo apt-get install build-essential autoconf libtool pkg-config python-opengl python-imaging python-pyrex python-pyside.qtopengl idle-python2.7 qt4-dev-tools qt4-designer libqtgui4 libqtcore4 libqt4-xml libqt4-test libqt4-script libqt4-network libqt4-dbus python-qt4 python-qt4-gl libgle3 python-dev libssl-dev
+# sudo easy_install greenlet
+# sudo easy_install gevent
+
+# pip install theanets
+# pip install bpython
