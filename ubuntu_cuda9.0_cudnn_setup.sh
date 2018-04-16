a4c7cd412271fa6f8200d60ae0cb9c9332377ac7
miyamoto <miyamotok0105@gmail.com>
2018-04-16 17:18:41 +0900

    update

--- ubuntu_cuda9.0_cudnn_setup.sh
+++ ubuntu_cuda9.0_cudnn_setup.sh
@@ -4,12 +4,19 @@ yes | sudo apt-get upgrade
 
 yes | sudo apt-get install g++ emacs tree curl git python-pip python2.7 python2.7-dev python3 python3-pip
 
+#https://developer.nvidia.com/cuda-toolkit
+#https://developer.nvidia.com/cudnn
 #https://docs.nvidia.com/deeplearning/sdk/cudnn-install/index.html
 #サイトからダウンロード
 yes | sudo dpkg -i cuda-repo-ubuntu1604-9-0-local_9.0.176-1_amd64.deb
 sudo apt-key add /var/cuda-repo-9-0-local/7fa2af80.pub
 yes | sudo apt-get update
 yes | sudo apt-get install cuda-9-0
+
+echo "#CUDA" >> ~/.bashrc
+export PATH="/usr/local/cuda/bin:$PATH"
+export LD_LIBRARY_PATH="/usr/local/cuda/lib64:$LD_LIBRARY_PATH"
+source ~/.bashrc
 
 tar -xzvf cudnn-9.0-linux-x64-v7.tgz
 sudo cp cuda/include/cudnn.h /usr/local/cuda/include
